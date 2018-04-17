861eef032e341d4a639f509e75dac76c4e3873b8
davidtm63 <davidtm63@gmail.com>
2018-04-17 14:16:43 +0100

    EJ 1 acabado

--- ej2/EjercicioPropiedades/main.m
+++ ej2/EjercicioPropiedades/main.m
@@ -24,7 +24,32 @@ int main(int argc, const char * argv[]) {
         //Crear persona con initConCodigo
         Persona * p2 = [[Persona alloc] initConCodigo:25 nombre:@"Juanjo" direccion:@"Casa ALain" telefono:@"66666" dni:@"123456F"];
         NSLog(@"Codigo: %d Nombre: %@ Telefono: %@ Direccion: %@ Dni: %@",p2.codigo,p2.nombre,p2.telefono,p2.direccion,p2.dni);
+        
+        Persona * persona1 = [[Persona alloc] initConCodigo:0 nombre:@"David" direccion:@"direccion1" telefono:@"tel1" dni:@"dni1"];
+        Persona * persona2 = [[Persona alloc] initConCodigo:1 nombre:@"Juan" direccion:@"direccion2" telefono:@"tel2" dni:@"dni2"];
+        Persona * persona3 = [[Persona alloc] initConCodigo:2 nombre:@"Pepe" direccion:@"direccion3" telefono:@"tel3" dni:@"dni3"];
+        Persona * persona4 = [[Persona alloc] initConCodigo:3 nombre:@"Rodrigo" direccion:@"direccion4" telefono:@"tel4" dni:@"dni4"];
+        Persona * persona5 = [[Persona alloc] initConCodigo:4 nombre:@"Emilio" direccion:@"direccion5" telefono:@"tel5" dni:@"dni5"];
+        
+        NSMutableArray * array = [[NSMutableArray alloc] init];
+        NSMutableDictionary * diccionario = [[NSMutableDictionary alloc] init];
+        [array addObject:persona1];
+        [array addObject:persona2];
+        [array addObject:persona3];
+        [array addObject:persona4];
+        [array addObject:persona5];
+        for(int i = 0;i<array.count;i++){
+            Persona *ptemp = array[i];
+            NSLog(@"%@", ptemp.nombre);
+            
+            [diccionario setObject:ptemp forKey:ptemp.nombre];
+        }
+        for(id nombre in diccionario){
+            Persona * per = diccionario[nombre];
+            NSLog(@"%@", per.telefono);
+        }
 
+        
     }
     return 0;
 }

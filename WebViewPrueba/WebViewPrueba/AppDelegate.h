7d10696ac12f22d3f87996b496abb8172177dd3e
davidtm63 <davidtm63@gmail.com>
2018-04-25 14:56:02 +0100

    :raised_hand:

---
+++ WebViewPrueba/WebViewPrueba/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewPrueba
+//
+//  Created by user137296 on 4/25/18.
+//  Copyright © 2018 user137296. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewPrueba/WebViewPrueba/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewPrueba
+//
+//  Created by user137296 on 4/25/18.
+//  Copyright © 2018 user137296. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
